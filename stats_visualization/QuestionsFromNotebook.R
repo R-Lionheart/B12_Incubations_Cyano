dfbc74490a8d8e36589118c2bb2901998b577d7a
Regina Lionheart <regina@lionhe.art>
2020-11-24 12:40:52 -0800

    Size fraction ratio graphs completed.

--- stats_visualization/QuestionsFromNotebook.R
+++ stats_visualization/QuestionsFromNotebook.R
@@ -18,8 +18,6 @@ replace_nonvalues <- function(x) (gsub(NaN, NA, x))
 ## DMB should be removed from averaged group, otherwise spikes the overall. 
 
 
-## Combine with bigger project, but keep the QC separate
-
 # NUTRIENTS: [DMBnoB12, noB12], both containing a f/2 spike 
 
 # NONUTRIENTS: [WB12, WDMB], no additional nutrients added.
@@ -32,9 +30,12 @@ replace_nonvalues <- function(x) (gsub(NaN, NA, x))
 
 # CONTROL: [Control], 100% in situ water from 25 m, incubated and then filtered.
 
-replace_nonvalues <- function(x) (gsub(NaN, NA, x))
-
 # Import files --------------------------------------------------
+Standards <- read.csv("https://raw.githubusercontent.com/IngallsLabUW/Ingalls_Standards/master/Ingalls_Lab_Standards_NEW.csv",
+                               stringsAsFactors = FALSE, header = TRUE) %>%
+  select(Compound.Name_old, Compound.Type) %>%
+  rename(Precursor.Ion.Name = Compound.Name_old)
+
 # Munge vitamin files
 Vitamins_5000QC <- read.csv("data_processed/Skyline_QE_QC_Output_Vitamins_5000QC.csv", stringsAsFactors = FALSE) %>%
   slice(-1:-9) %>%
@@ -61,7 +62,7 @@ Vitamins_1500QC <- read.csv("data_processed/Skyline_QE_QC_Output_Vitamins_1500QC
          Dataset = "Vitamins1500")
 
 # Munge full Incubations dataset
-Incubations <- read.csv("data_processed/MSDial_QE_QC_Output_B12-Incubations_2020-10-12.csv",
+Incubations <- read.csv("data_processed/MSDial_QE_QC_Output_B12-Incubations.csv",
                         stringsAsFactors = FALSE) %>%
   slice(-1:-6) %>%
   select(-c(Description, Value)) %>%
@@ -91,7 +92,7 @@ ggplot(All.Vitamins, aes(x = QC.Level, y = n, fill = Has.Flag)) +
   annotate("text", x = 6000, y = 600, label = "651 + 273 = 924") +
   ggtitle("Comparison of Area Min Flags in QC Levels: Vitamins")
 
-ggsave("figures/QC_Comparison.png")
+#ggsave("figures/QC_Comparison.png")
 
 
 ## Combine with full Cyano dataset --------------------------------------------------
@@ -130,24 +131,22 @@ filter(!Precursor.Ion.Name == "B2-IS" && Dataset == "Vitamins",
 write.csv(Complete.Dataset.Avg, "data_intermediate/Vitamins_Incubations_CompleteDatasetAvg.csv")
 
 
- 
 # Notebook questions --------------------------------------------------------
 # Which compounds exist in which size fraction?
 Which.Cmpds <- Complete.Dataset %>%
   group_by(Precursor.Ion.Name, Size.Fraction) %>%
   mutate(
-    NumberOfSamples = n(),
+    Number.Of.Samples = n(),
     Number.Missing = sum(is.na(Area.with.QC)),
-    Percent.Present = 1 - (sum(is.na(Area.with.QC))/NumberOfSamples),
+    Percent.Present = 1 - (sum(is.na(Area.with.QC))/Number.Of.Samples),
     Over.Half.Missing = ifelse(Percent.Present >= 0.5, FALSE, TRUE)
   ) %>%
   ungroup()
 
-ggplot(Which.Cmpds, aes(x = reorder(Precursor.Ion.Name, -Percent.Present), 
+ggplot(Which.Cmpds, aes(x = reorder(Precursor.Ion.Name, -Percent.Present),
                         y = Percent.Present)) +
-  facet_wrap(~Size.Fraction) +
-  geom_bar(position = "dodge", stat = "identity", 
-           aes(fill = as.numeric(row.names(Which.Cmpds)) %% 2 == 0)) +
+  facet_wrap(~Size.Fraction + Eddy) +
+  geom_bar(position = "dodge", stat = "identity") +
   scale_fill_discrete(guide = "none") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.25)) +
   scale_x_discrete(name ="Precursor.Ion.Name") +
@@ -155,12 +154,33 @@ ggplot(Which.Cmpds, aes(x = reorder(Precursor.Ion.Name, -Percent.Present),
   ggtitle("Which Compounds Exist in Which Size Fractions?")
 ggsave("figures/Which.Compounds.Present.png")
 
-Table.DF <- Which.Cmpds %>%
-  select(Precursor.Ion.Name, Percent.Present) %>%
-  filter(Percent.Present == 1 | Percent.Present == 0) %>%
-  unique() %>%
-  arrange(Percent.Present)
-write.csv(Table.DF, "data_intermediate/WhichCompoundsTable.csv")
+# Which compounds were completely in one or the other size fraction?
+WhichCompounds <- function(df, EddyDirection) {
+  Table.DF <- df %>%
+    filter(Eddy == EddyDirection) %>%
+    group_by(Precursor.Ion.Name) %>%
+    mutate(Number.Of.Samples = n(),
+           Number.Missing = sum(is.na(Area.with.QC.mean))) %>%
+    filter(Number.Of.Samples != Number.Missing) %>%
+    select(-c(Area.mean, Number.Of.Samples, Number.Missing, Dataset)) %>%
+    group_by(Precursor.Ion.Name, Size.Fraction) %>%
+    mutate(Is.Present = ifelse(is.na(Area.with.QC.mean), TRUE, FALSE)) %>%
+    group_by(Precursor.Ion.Name, Size.Fraction) %>%
+    mutate(Group.Present = ifelse(any(Is.Present == FALSE), TRUE, FALSE)) %>%
+    ungroup() %>%
+    group_by(Precursor.Ion.Name) %>%
+    mutate(Filter.Check = ifelse(any(Group.Present == FALSE), TRUE, FALSE)) %>%
+    filter(Filter.Check == TRUE) %>%
+    filter(Group.Present == TRUE) %>%
+    select(Precursor.Ion.Name, Size.Fraction, Eddy) %>%
+    unique()
+  return(Table.DF)
+}
+
+Table.DF.Cyclonic <- WhichCompounds(Complete.Dataset.Avg, "Cyclonic")
+Table.DF.Anticyclonic <- WhichCompounds(Complete.Dataset.Avg, "Anticyclonic")
+write.csv(Table.DF.Cyclonic, "data_intermediate/WhichCompoundsSizeFraction_Cyclonic.csv")
+write.csv(Table.DF.Anticyclonic, "data_intermediate/WhichCompoundsSizeFraction_Anticyclonic.csv")
 
 ## Do nutrients change with additions/size fractions?
 Nutrient.Change <- Complete.Dataset.Avg %>%
@@ -169,14 +189,15 @@ Nutrient.Change <- Complete.Dataset.Avg %>%
   unique() %>%
   filter(!is.na(Area.with.QC.mean)) %>%
   mutate(Area.with.QC.mean = as.numeric(Area.with.QC.mean)) %>%
-  mutate(Dataset.Position = ifelse(Area.with.QC.mean > 633000, 
-                                   "Higher.Average.Area", "Lower.Average.Area"))
+  mutate(Dataset.Position = ifelse(Area.with.QC.mean > 633000,
+                                   "Higher.Average.Area", "Lower.Average.Area")) %>%
+  arrange(desc(Area.with.QC.mean)) %>%
+  mutate(Precursor.Ion.Name=factor(Precursor.Ion.Name, levels=Precursor.Ion.Name))
 
-ggplot(Nutrient.Change, aes(x = reorder(Precursor.Ion.Name, -Area.with.QC.mean), 
+ggplot(Nutrient.Change, aes(x = reorder(Precursor.Ion.Name, -Area.with.QC.mean),
                             y = Area.with.QC.mean, fill = Binned.Group)) +
   geom_bar(position = "dodge", stat = "identity") +
-  coord_flip() +
-  facet_wrap(~Size.Fraction + Dataset.Position, scales = "free") +
+  facet_wrap(~Dataset.Position + Size.Fraction, scales = "free") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
   scale_y_continuous("QC'd Average Area") +
   scale_x_discrete("Precursor Ion Name") +
@@ -184,56 +205,78 @@ ggplot(Nutrient.Change, aes(x = reorder(Precursor.Ion.Name, -Area.with.QC.mean),
 ggsave("figures/DoNutrientsChange.png")
 
 
-## Do compound proportions stay the same w/ DSW?
+## Do compound proportions stay the same w/ DSW, based on compound type?
 DSWproportions <- Complete.Dataset.Avg %>%
+  left_join(Standards) %>%
   filter(Eddy == "Anticyclonic",
          Size.Fraction == "Large.Filter") %>%
-  select(Precursor.Ion.Name, Binned.Group, Area.with.QC.mean, Size.Fraction, Eddy) %>%
-  unique() 
+  ungroup() %>%
+  select(Compound.Type, Binned.Group, Area.with.QC.mean, Size.Fraction, Eddy)
 
-ggplot(DSWproportions, aes(x = Binned.Group, y = Area.with.QC.mean, fill = Precursor.Ion.Name)) +
-  geom_bar(position = "fill", stat = "identity") +
+ggplot(DSWproportions, aes(x = reorder(Binned.Group, -Area.with.QC.mean),
+                           y = Area.with.QC.mean, fill = Compound.Type)) +
+  geom_bar(stat = "identity", position = "fill") +
   coord_flip()
-ggsave("figures/DoCompoundProportionsChange_BetaineCholine.png")
-
-# Hopefully will work at some point...
-# ggplot(data=DSWproportions, aes(x=Area.with.QC.mean, group=Precursor.Ion.Name, fill=Precursor.Ion.Name)) +
-#   facet_wrap(~Binned.Group) +
-#   geom_density(adjust=1.5, alpha=.4) 
-# ggsave("figures/DensityProportions_BetaineCholine.png")
+ggsave("figures/DoCompoundProportionsChange_CmpdType.png")
 
 
 ## Ask a compound: how much of all of the compound are in large/small size fraction? Assume n peak area 0.2
 # + n peak area 5 = 100%.
 
-SF.ratios <- Complete.Dataset.Avg %>%
+plotchart <- function(data) {
+  # Quick plotting function for size fraction ratios.
+  plot <- ggplot(data) +
+    aes(x = reorder(Precursor.Ion.Name, Order), 
+        y = Percent,
+        fill = Size.Fraction) +
+    geom_bar(stat = "identity", position = "fill") +
+    coord_flip() +
+    ggtitle(paste(unique(data[[1]][1])))
+  
+  print(plot)
+}
+
+
+# Split by eddy orientation
+Size.Fraction.Ratios <- Complete.Dataset.Avg %>%
   ungroup() %>%
   filter(Eddy == "Anticyclonic") %>%
   mutate_at(c("Area.with.QC.mean"), replace_nonvalues) %>%
   mutate(Area.with.QC.mean = as.numeric(Area.with.QC.mean)) %>%
-  group_by(Precursor.Ion.Name, Size.Fraction) %>%
+  group_by(Precursor.Ion.Name, Binned.Group, Size.Fraction) %>%
   mutate(Sum.per.SF = sum(Area.with.QC.mean, na.rm = TRUE)) %>%
   select(Precursor.Ion.Name, Eddy, Size.Fraction, Sum.per.SF, Binned.Group) %>%
   unique() %>%
   filter(!Sum.per.SF == 0) %>%
   group_by(Precursor.Ion.Name) %>%
   add_tally() %>%
-  filter(!n == 5) 
+  filter(!n == 5) %>%
+  select(-c(Eddy, n)) %>%
+  ungroup()
+  
+SF.Ratios.Plot <- Size.Fraction.Ratios %>%
+  complete(nesting(Binned.Group, Precursor.Ion.Name), Size.Fraction, fill = list(Sum.per.SF = 0)) %>%
+  group_by(Binned.Group, Precursor.Ion.Name) %>%
+  mutate(Percent = Sum.per.SF / sum(Sum.per.SF)) %>%
+  mutate(Large.Filter.Percent = Percent[Size.Fraction == "Large.Filter"]) %>%
+  arrange(Binned.Group, desc(Large.Filter.Percent), desc(Precursor.Ion.Name), Size.Fraction) %>%
+  ungroup() %>%
+  mutate(Order = row_number()) %>%
+  group_by(Binned.Group) %>%
+  group_split() 
 
-SF.ratios$Precursor.Ion.Name <- factor(SF.ratios$Precursor.Ion.Name, levels=rev(unique(SF.ratios$Precursor.Ion.Name)))
+names(SF.Ratios.Plot) <- unique(Size.Fraction.Ratios$Binned.Group)
 
-ggplot(SF.ratios, aes(x = Sum.per.SF, y = Precursor.Ion.Name, fill = Size.Fraction)) +
-  facet_wrap(~Binned.Group) +
-  geom_bar(position = "fill", stat = "identity") +
-  ggtitle("Anticyclonic: How much of the compound is in the large and small size fraction?")
-ggsave("figures/HowMuchinSizeFractions.png")
+plotlist <- lapply(SF.Ratios.Plot, plotchart)
+
 
 ## Which ones were completely in one or the other
-# Check these graphs!! last one is artifice
+## Check these graphs!! Reorder them.
 ## Box and whisker for how these plots are behaving similarly. Which compounds are behaving the same in all treatments?
 ## Add line to that to show moving directions of compounds.
 ## Cluster them to see whos moving together. Stuff is always going up in deep sea water? Always going down in Time0?
 ## Also just do a set of amino acids for all the graphs. 
 
 ## Good ionizers will be more abundant. 
+## Does the pathway change under different scenarios?
 ## Ask Sonya (for example) who makes ectoine? 
